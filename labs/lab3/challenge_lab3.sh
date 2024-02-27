2fa2a547b18df9add2a56a5b3c770baa4c4a8a81
extortiun <rtorres2@students.pccc.edu>
2024-02-27 12:38:43 -0500

    Lab 3 Finished

---
+++ labs/lab3/challenge_lab3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "The current date is:"
+date -R
+echo -e "\nInformation about my system:"
+uname -a
+echo -e "\nRam Information"
+free
+echo -e "\nChallenge Question Complete"
+
---
+++ labs/lab3/lab3-script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bas
+#Author: Roy Torres
+#Date: current date in format Feb 24th 24
+#Description: This is a basic script that
+#uses environment variables and the date command
+echo "This is a shell script,"
+echo -n "This script will print the current date: "
+date
+echo "This is the machine's name:"
+echo $HOSTNAME
+echo "This is the current user:"
+echo $USER
+
