784248dcbfb1396e37e2fffd1a09500a3a45fae9
extortiun <rtorres2@students.pccc.edu>
2024-10-10 19:56:20 -0400

    Lab 3 Finished

---
+++ labs/lab3/challenge_lab3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: Roy Torres
+# Date: 10/10/24
+# Description: challenge question solution
+
+echo "The current date is:"
+date -R
+echo -e "\nInformation about my system:"
+uname -a
+echo -e "\nRam Information"
+free
+echo -e "\nDisk Space Usage"
+df
+figlet DONE
+echo -e "\nChallenge Question Complete"
---
+++ labs/lab3/lab3.md
@@ -0,0 +1,19 @@
+---
+Student: Roy Torres
+semester: Fall 24
+Assignment: Lab 3
+---
+
+# Lab 3 Submission
+
+## Question 2
+![q2](q2.1.png)
+
+## Question 3
+![q3](q3.1.png)
+
+## Challenge Question
+![q4](q4.png)
+
+## Script Source code:
+[lab3](lab3-script.sh)
